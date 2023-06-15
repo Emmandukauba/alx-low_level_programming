21417fe7511829ed509006e83aec9d01b95ffce2
Emmandukauba <emmandukauba@example.com>
2023-06-15 08:05:39 -0700

    my first printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * description: 'the printf was to print a given phrase'
+ * the parameter to be used: i catually dont know
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+i{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
