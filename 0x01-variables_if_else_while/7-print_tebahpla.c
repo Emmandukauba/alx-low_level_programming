1e3a8e1cdbdbe81d0de3c87cd2c73ec293ea6371
Emmandukauba <emmandukauba@example.com>
2023-06-16 09:11:03 -0700

    print in small cases

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing in lower cases starting from behind,
+ * then i added a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
