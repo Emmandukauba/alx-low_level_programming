af103b1d7eaa628a6f7ed3f418a006337d4a9876
Emmandukauba <emmandukauba@example.com>
2023-06-16 09:30:40 -0700

    printing alphabets in C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Printing alphabeths in progress here
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char arinz[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
 	{
-		putchar(alp[i]);
+		putchar(arinz[i]);
 	}
 	putchar('\n');
 	return (0);
