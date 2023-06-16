28937bdbac87701ea16a66d6ae59bb27d42f9d12
Emmandukauba <emmandukauba@example.com>
2023-06-16 09:15:04 -0700

    this is cool

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - if you cant read my code it is becaause i dont want you to
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
