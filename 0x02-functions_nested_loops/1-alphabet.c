5d4c9a783df25b1218a69a68ac17f871b5546991
Emmandukauba <emmandukauba@example.com>
2023-06-20 02:32:22 -0700

    prnting alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+	/**
+	 * description print_alphabet - printing in lower cases abc
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char arinze;
+
+
+		for (arinze = 'a'; arinze <= 'z'; arinze++)
+			_putchar(arinze);
+
+
+		_putchar('\n');
+	}
